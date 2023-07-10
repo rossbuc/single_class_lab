2945d3020c8ebaa135cc9ca9a40bcf215cd17c6b
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-10 14:11:09 +0100

    Setup Student class with init, talk, and favourite language methods

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,1 +1,10 @@
+class Student: 
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
 
+    def talk():
+        return "I can talk"
+
+    def say_favourite_language(language):
+        return "I love " + language
